7b26ac78c997e8cec159b9e30bea4e54fcc28fc1
Nebras7787 <nbrasalhbshy@gmail.com>
2025-07-19 14:56:37 +0300

    zhh

---
+++ chat/src/chat/chat_server.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ *
+ */
+public class chat_server {
+
+    public static void main(String[] args) throws IOException {
+        Server.main(args);
+    }
+}
+
+
